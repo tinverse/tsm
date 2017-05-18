cd5dc908578a4b85262e0faa8d3d4cb16c3f9f56
Sriram Sundararajan <sriram@tinverse.com>
2017-05-17 23:09:03 -0700

    TransitionT is templated by State and Event types.
    Types like Transition, TransitionTable etc. have been moved to
    StateMachine.h since these depend on concrete State and Event types.

--- Transition.h
+++ Transition.h
@@ -1,6 +1,4 @@
 #pragma once
-#include "Event.h"
-#include "State.h"
 #include "hash.h"
 
 #include <glog/logging.h>
@@ -19,36 +17,16 @@ public:
         , onEvent(event)
     {
     }
-    TransitionT(const TransitionT&) = default;
     virtual ~TransitionT()          = default;
+
+    TransitionT(const TransitionT&) = default;
+
+    virtual void execute() { DLOG(INFO) << __PRETTY_FUNCTION__ << std::endl; }
 
     std::shared_ptr<State> fromState;
     std::shared_ptr<State> toState;
     Event onEvent;
 };
 
-class Transition : public TransitionT<State, Event>
-{
 
-public:
-    Transition(std::shared_ptr<State> fromState,
-               Event event,
-               std::shared_ptr<State> toState)
-        : TransitionT(fromState, event, toState)
-    {
-    }
-
-    Transition(const Transition&) = default;
-    virtual ~Transition()         = default;
-
-    virtual void execute() { DLOG(INFO) << __PRETTY_FUNCTION__ << std::endl; }
-};
-
-typedef std::pair<std::shared_ptr<State>, Event> StateEventPair;
-typedef std::unordered_map<StateEventPair, Transition> TransitionTable;
-
-inline bool operator==(const StateEventPair& p1, const StateEventPair& p2)
-{
-    return (p1.first->id == p2.first->id) && (p1.second.id == p2.second.id);
-}
 
