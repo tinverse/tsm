0cdc89968b414271ec34293573f5a78f9d9ea142
Sriram Sundararajan <sriram@tinverse.com>
2018-08-15 11:23:07 -0700

    Fixed hash function. Works with both clang and gcc

--- hash.h
+++ hash.h
@@ -1,69 +1,16 @@
 #pragma once
-// Source: StackOverflow
-#undef get16bits
-#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) || \
-    defined(_MSC_VER) || defined(__BORLANDC__) || defined(__TURBOC__)
-#define get16bits(d) (*((const uint16_t*)(d)))
-#endif
-
-#if !defined(get16bits)
-#define get16bits(d)                               \
-  ((((uint32_t)(((const uint8_t*)(d))[1])) << 8) + \
-   (uint32_t)(((const uint8_t*)(d))[0]))
-#endif
 
 namespace std {
-template <>
+template<>
 struct hash<StateEventPair> {
   size_t operator()(const StateEventPair& s) const {
-    StateEventPair s1 = s;
-    uint32_t len = sizeof(s1);
-    const char* data = reinterpret_cast<const char*>(&s1);
-    uint32_t hash = len, tmp;
-    int rem;
-
-    if (len <= 0 || data == NULL) return 0;
-
-    rem = len & 3;
-    len >>= 2;
-
-    /* Main loop */
-    for (; len > 0; len--) {
-      hash += get16bits(data);
-      tmp = (get16bits(data + 2) << 11) ^ hash;
-      hash = (hash << 16) ^ tmp;
-      data += 2 * sizeof(uint16_t);
-      hash += hash >> 11;
-    }
-
-    /* Handle end cases */
-    switch (rem) {
-      case 3:
-        hash += get16bits(data);
-        hash ^= hash << 16;
-        hash ^= ((signed char)data[sizeof(uint16_t)]) << 18;
-        hash += hash >> 11;
-        break;
-      case 2:
-        hash += get16bits(data);
-        hash ^= hash << 11;
-        hash += hash >> 17;
-        break;
-      case 1:
-        hash += (signed char)*data;
-        hash ^= hash << 10;
-        hash += hash >> 1;
-    }
-
-    /* Force "avalanching" of final 127 bits */
-    hash ^= hash << 3;
-    hash += hash >> 5;
-    hash ^= hash << 4;
-    hash += hash >> 17;
-    hash ^= hash << 25;
-    hash += hash >> 6;
-
-    return hash;
+    std::shared_ptr<State> statePtr = s.first;
+    Event event = s.second;
+    size_t address = reinterpret_cast<size_t>(statePtr.get());
+    size_t id = event.id;
+    size_t hash_value =  std::hash<int>{}(address) ^ (std::hash<size_t>{}(id) << 1); 
+    LOG(ERROR) << statePtr->name << ":" << event.id << ":" << hash_value;
+    return hash_value;
   }
 };
 }
