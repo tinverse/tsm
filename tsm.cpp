e596110ba4f1a616c00637164d9edc72db26a081
Sriram Sundararajan <sriram@tinverse.com>
2018-09-12 16:59:24 -0700

    remove dependency on tsm.cpp. Roll hash.h into tsm.h

--- tsm.cpp
+++ tsm.cpp
@@ -7,9 +7,3 @@
 #include "tsm.h"
 
 using tsm::StateEventPair;
-
-bool
-operator==(const StateEventPair& s1, const StateEventPair& s2)
-{
-    return (s1.first.get() == s2.first.get()) && (s1.second.id == s2.second.id);
-}
