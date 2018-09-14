87ef64d5f08d40371c0ab752f1d6a383daf9996c
Sriram Sundararajan <sriram@tinverse.com>
2018-09-13 19:19:50 -0700

    Moved GarageDoorSM to a header file

--- test/GarageDoorSM.cpp
+++ test/GarageDoorSM.cpp
@@ -1,60 +1,6 @@
-#include <glog/logging.h>
-#include <gtest/gtest.h>
+#include "GarageDoorSM.h"
 
-#include "StateMachineTest.h"
-
-using tsm::Event;
-using tsm::EventQueue;
-using tsm::OrthogonalHSM;
-using tsm::State;
-using tsm::StateMachine;
-
-using tsmtest::StateMachineTest;
-
-struct GarageDoorSM : public StateMachineTest<GarageDoorSM>
-{
-    GarageDoorSM() = delete;
-
-    GarageDoorSM(std::string name, EventQueue<Event>& eventQueue)
-      : StateMachineTest<GarageDoorSM>(name, eventQueue)
-      , doorOpen(std::make_shared<State>("Door Open"))
-      , doorOpening(std::make_shared<State>("Door Opening"))
-      , doorClosed(std::make_shared<State>("Door Closed"))
-      , doorClosing(std::make_shared<State>("Door Closing"))
-      , doorStoppedClosing(std::make_shared<State>("Door Stopped Closing"))
-      , doorStoppedOpening(std::make_shared<State>("Door Stopped Opening"))
-    {
-        // TransitionTable
-        add(doorClosed, click_event, doorOpening);
-        add(doorOpening, topSensor_event, doorOpen);
-        add(doorOpen, click_event, doorClosing);
-        add(doorClosing, bottomSensor_event, doorClosed);
-        add(doorOpening, click_event, doorStoppedOpening);
-        add(doorStoppedOpening, click_event, doorClosing);
-        add(doorClosing, obstruct_event, doorStoppedClosing);
-        add(doorClosing, click_event, doorStoppedClosing);
-        add(doorStoppedClosing, click_event, doorOpening);
-        add(doorClosed, click_event, doorOpening);
-    }
-
-    virtual ~GarageDoorSM() = default;
-
-    shared_ptr<State> getStartState() const override { return doorClosed; }
-
-    // States
-    shared_ptr<State> doorOpen;
-    shared_ptr<State> doorOpening;
-    shared_ptr<State> doorClosed;
-    shared_ptr<State> doorClosing;
-    shared_ptr<State> doorStoppedClosing;
-    shared_ptr<State> doorStoppedOpening;
-
-    // Events
-    Event click_event;
-    Event bottomSensor_event;
-    Event topSensor_event;
-    Event obstruct_event;
-};
+using tsmtest::GarageDoorSM;
 
 struct TestGarageDoorSM : public ::testing::Test
 {
---
+++ test/GarageDoorSM.h
@@ -0,0 +1,53 @@
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#include "StateMachineTest.h"
+
+using tsmtest::StateMachineTest;
+
+namespace tsmtest {
+struct GarageDoorSM : public StateMachineTest<GarageDoorSM>
+{
+    GarageDoorSM() = delete;
+
+    GarageDoorSM(std::string name, EventQueue<Event>& eventQueue)
+      : StateMachineTest<GarageDoorSM>(name, eventQueue)
+      , doorOpen(std::make_shared<State>("Door Open"))
+      , doorOpening(std::make_shared<State>("Door Opening"))
+      , doorClosed(std::make_shared<State>("Door Closed"))
+      , doorClosing(std::make_shared<State>("Door Closing"))
+      , doorStoppedClosing(std::make_shared<State>("Door Stopped Closing"))
+      , doorStoppedOpening(std::make_shared<State>("Door Stopped Opening"))
+    {
+        // TransitionTable
+        add(doorClosed, click_event, doorOpening);
+        add(doorOpening, topSensor_event, doorOpen);
+        add(doorOpen, click_event, doorClosing);
+        add(doorClosing, bottomSensor_event, doorClosed);
+        add(doorOpening, click_event, doorStoppedOpening);
+        add(doorStoppedOpening, click_event, doorClosing);
+        add(doorClosing, obstruct_event, doorStoppedClosing);
+        add(doorClosing, click_event, doorStoppedClosing);
+        add(doorStoppedClosing, click_event, doorOpening);
+        add(doorClosed, click_event, doorOpening);
+    }
+
+    virtual ~GarageDoorSM() = default;
+
+    shared_ptr<State> getStartState() const override { return doorClosed; }
+
+    // States
+    shared_ptr<State> doorOpen;
+    shared_ptr<State> doorOpening;
+    shared_ptr<State> doorClosed;
+    shared_ptr<State> doorClosing;
+    shared_ptr<State> doorStoppedClosing;
+    shared_ptr<State> doorStoppedOpening;
+
+    // Events
+    Event click_event;
+    Event bottomSensor_event;
+    Event topSensor_event;
+    Event obstruct_event;
+};
+} // namespace tsmtest
