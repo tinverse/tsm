aef1395332427145fbad5e61cd0e26585f83252b
Sriram Sundararajan <sriram@tinverse.com>
2018-09-27 17:57:42 -0700

    Added code coverage supporte

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,22 +1,15 @@
                                         ; -*- mode: Lisp -*-
-(setq cmake-ide-build-dir '"build/")
-
-(c++-mode . ((helm-make-build-dir . "build/")
-             (helm-make-arguments . "-j4")
-              (indent-tabs-mode . nil)
-              (tab-width . 4)
-              (c-basic-offset . 'tab-width)))
-
-(cmake-mode . ((cmake-ide-project-dir . "./")
+(nil . ((cmake-ide-project-dir . "./")
          (cmake-ide-build-dir . "build/")
-         (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_DEPENDENCIES=OFF")))
-
-(nil . ((projectile-project-name . "tsm")
-                    (projectile-project-run-cmd . "ctest")
-                    (projectile-project-test-cmd . "ctest")))
-
-(nil . ((helm-ctest-build-dir . "build/")
+         (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_DEPENDENCIES=OFF")
+        (helm-make-build-dir . "build/")
+        (helm-make-arguments . "-j4")
+        (indent-tabs-mode . nil)
+        (tab-width . 4)
+        (c-basic-offset . 'tab-width)
+        (projectile-project-name . "tsm")
+        (projectile-project-run-cmd . "ctest")
+        (projectile-project-test-cmd . "ctest")
+        (helm-ctest-build-dir . "build/")
         (helm-make-build-dir . "build/")
         (helm-make-arguments . "-j4")))
-
-(nil . ((desktop-save-mode 1)))
