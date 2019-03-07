34f655bbb39299adef85cb33345aef4d3b674a08
Sriram Sundararajan <sriram@tinverse.com>
2019-03-07 07:05:53 -0800

    Fix for building docs on TravisCI
    Docs are built and pushed to github.io by default every time on the CI
    server. This option should be on while building on TravisCI..
    
    Also added a condition on .travis.yml to check if the doc output folder
    exists.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 mkdir build
 cd build
-cmake -GNinja -DBUILD_COVERAGE=ON -DBUILD_DEPENDENCIES=OFF ..
+cmake -GNinja -DBUILD_COVERAGE=ON -DBUILD_DEPENDENCIES=OFF -DBUILD_DOCUMENTATION=ON ..
 cmake --build .
