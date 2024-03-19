ee573f9163a7b969a585c77e9ca92770528c63b6
Sriram Sundararajan <1046118+dibsonmuad@users.noreply.github.com>
2024-03-19 01:50:32 -0700

    update example

--- examples/hello_tsm/main.cpp
+++ examples/hello_tsm/main.cpp
@@ -1,45 +1,47 @@
 #include <tsm.h>
 
-using tsm::Event;
-using tsm::State;
+// Example TCP socket state machine
+namespace tcp_socket {
+    Socket {
+        // Events - sock_open, bind, listen, connect, accept, close;
+        struct sock_open {};
+        struct bind {};
+        struct listen {};
+        struct connect {};
+        struct accept {};
+        struct close {};
 
-struct SocketSM : tsm::Hsm<SocketSM>
-{
-  public:
-    SocketSM()
-      : tsm::Hsm<SocketSM>()
-    {
-        IHsm::setStartState(&Closed);
+        // States - Closed, Ready, Bound, Open, Listening;
+        struct Closed {};
+        struct Ready {};
+        struct Bound {};
+        struct Open {};
+        struct Listening {};
 
-        add(Closed, sock_open, Ready);
-        add(Ready, connect, Open);
-        add(Ready, bind, Bound);
-        add(Bound, listen, Listening);
-        add(Listening, accept, Listening);
-        add(Listening, close, Closed);
-        add(Open, close, Closed);
-    }
+        using transitions = std::tuple<Transition<Closed, sock_open, Ready>,
+                                       Transition<Ready, bind, Bound>,
+                                       Transition<Bound, listen, Listening>,
+                                       Transition<Ready, connect, Open>,
+                                       Transition<Open, close, Closed>,
+                                       Transition<Bound, close, Closed>,
+                                       Transition<Listening, close, Closed>,
+                                       Transition<Open, accept, Open>>;
+        // ip address, port, backlog
+        std::string ip;
+        int port;
+        int backlog;
+    };
+} // namespace tcp_socket
 
-    // Events
-    Event sock_open, bind, listen, connect, accept, close;
 
-    // States
-    State Closed, Ready, Bound, Open, Listening;
-};
-
-using SocketHsm = tsm::SingleThreadedHsm<SocketSM>;
+using SocketHsm = tsm::SingleThreadedHsm<tcp_socket::Socket>;
 
 int
 main()
 {
     SocketHsm sm;
-
-    // Important!!
-    sm.startSM();
-
     // send events... and step the sm
-    sm.sendEvent(sm.sock_open);
+    sm.sendEvent(tcp_socket::sock_open{});
+    // Do other stuff and then step the sm
     sm.step();
-
-    sm.stopSM();
 }
